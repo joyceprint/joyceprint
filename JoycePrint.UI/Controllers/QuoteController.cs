2595e634026a0f30bd2b93abccea64400740063c
polydegmon <polydegmon@gmail.com>
2016-10-22 20:05:04 -0600

    Quote page styled with form control labels

---
+++ JoycePrint.UI/Controllers/QuoteController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace JoycePrint.UI.Controllers
+{
+    [Route("/")]
+    public class QuoteController : Controller
+    {        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        //[HttpPost]
+        //public ActionResult Index()
+        //{
+        //    return View("Index");
+        //}
+    }
+}
