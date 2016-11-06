d6dfdf70d85b6fe3982145b65d49aaf5a202b667
polydegmon <polydegmon@gmail.com>
2016-11-05 20:03:03 -0600

    Bootstrap removed

--- JoycePrint.Web/App_Start/BundleConfig.cs
+++ JoycePrint.Web/App_Start/BundleConfig.cs
@@ -12,14 +12,14 @@ namespace JoycePrint.UI
             // {version} can be used to specify the jQuery version, the token has to appear last in the file name
             bundles.Add(new ScriptBundle("~/Scripts/joyceprintjs").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/bootstrap.min.js",
+                //"~/Scripts/bootstrap.min.js",
                 "~/Scripts/joyceprint/joyceprint.js"));
 
             // Create the style bundle, using a bundle instead of a style bundle allows us to mix css and less files.
             // The less file is translated to css by the LessTransform class passed into the bundle
             bundles.Add(new Bundle("~/Content/css/joyceprintcss", new LessTransform()).Include(
-                "~/Content/css/bootstrap.min.css",
-                "~/Content/css/bootstrap-theme.min.css",
+                //"~/Content/css/bootstrap.min.css",
+                //"~/Content/css/bootstrap-theme.min.css",
                 "~/Content/css/joyceprint/joyceprint.less"
                 ));
             
