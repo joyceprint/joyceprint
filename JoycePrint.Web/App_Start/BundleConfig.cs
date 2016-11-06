28824187a09da17825564274ef66da3cde869d98
polydegmon <polydegmon@gmail.com>
2016-11-05 20:31:39 -0600

    Materialize added. Sticky footer created

--- JoycePrint.Web/App_Start/BundleConfig.cs
+++ JoycePrint.Web/App_Start/BundleConfig.cs
@@ -10,19 +10,18 @@ namespace JoycePrint.UI
 
             // Create the script bundle
             // {version} can be used to specify the jQuery version, the token has to appear last in the file name
-            bundles.Add(new ScriptBundle("~/Scripts/joyceprintjs").Include(
+            bundles.Add(new ScriptBundle("~/js/joyceprintjs").Include(
                 "~/Scripts/jquery-{version}.js",
-                //"~/Scripts/bootstrap.min.js",
+                "~/Scripts/materialize.min.js",
                 "~/Scripts/joyceprint/joyceprint.js"));
 
             // Create the style bundle, using a bundle instead of a style bundle allows us to mix css and less files.
             // The less file is translated to css by the LessTransform class passed into the bundle
-            bundles.Add(new Bundle("~/Content/css/joyceprintcss", new LessTransform()).Include(
-                //"~/Content/css/bootstrap.min.css",
-                //"~/Content/css/bootstrap-theme.min.css",
+            bundles.Add(new Bundle("~/css/joyceprintcss", new LessTransform()).Include(
+                "~/Content/css/materialize.min.css",
                 "~/Content/css/joyceprint/joyceprint.less"
                 ));
-            
+
             // Optimization for script and style bundles.
             // In debug mode the scripts will not be minified in the browser
 #if DEBUG
