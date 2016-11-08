454c41af0459d7660c068cddf69c2a9e49c188e7
polydegmon <polydegmon@gmail.com>
2016-11-07 21:33:12 -0700

    Bootstrap references changed to materialize
    Redundant code and styles removed

--- JoycePrint.Web/Scripts/joyceprint/joyceprint.js
+++ JoycePrint.Web/Scripts/joyceprint/joyceprint.js
@@ -1,77 +1,10 @@
 ﻿$(document).ready(function () {
-    toggleNavigationMenu();
 
-    contactUsValidation();
+    // Carousel init
+    $('.carousel').carousel();
 
-    handleCssHtmlRestriction();
+    // Slider init
+    $('.carousel-slider').slider({ full_width: true });
+
+    //$('.carousel.carousel-slider').carousel({ full_width: true });
 });
-
-/*
- * Creates an event that will add the touched class to required fields
- * This is required as the :visited pseudo class only applies to anchor tags
- */
-function handleCssHtmlRestriction() {
-
-    // use $.fn.one here to fire the event only once.
-    $(':required').one('blur keydown', function () {
-        $(this).addClass('touched');
-    });
-}
-
-/*
- * Applies the classes used for validation depending on whether the target is [ :valid | :invalid ]
- */
-function contactUsValidation() {
-
-    $(':required').on("focusout keyup change", function (e) {
-
-        // If we tabbed into the input we do not want to perform validation
-        if (e.originalEvent.code === "Tab")
-            return;
-
-        var input = $(e.target);
-        var symbolContainer = $(e.target).prev();
-        var symbol = $(symbolContainer).children().first();
-
-        if ($(input).is(':invalid')) {
-            $(symbol).removeAttr("class");
-            $(symbol).addClass("glyphicon glyphicon-remove-circle text-danger");
-
-            if ($(input).is("textarea")) {
-                $(input).addClass("bg-danger");
-            } else {
-                $(symbolContainer).removeAttr("class");
-                $(symbolContainer).addClass("input-group-addon invalid");
-            }
-        } else if ($(input).is(':valid')) {
-            $(symbol).removeAttr("class");
-            $(symbol).addClass("glyphicon glyphicon-ok-circle text-success");
-
-            if ($(input).is("textarea")) {
-                $(input).removeClass("bg-danger");
-            } else {
-                $(symbolContainer).removeAttr("class");
-                $(symbolContainer).addClass("input-group-addon valid");
-            }
-        }
-    });
-}
-
-/*
- * Toggle the navigation menu so the active page reflects the page the user is currently on
- */
-function toggleNavigationMenu() {
-
-    // Find and remove the active class
-    $("#jpNav").find(".active").removeClass("active");
-
-    if ($("#home").length > 0) {
-        $("#jpNav #liHome").addClass("active");
-    } else if ($("#quote").length > 0) {
-        $("#jpNav #liQuote").addClass("active");
-    } else if ($("#services").length > 0) {
-        $("#jpNav #liServices").addClass("active");
-    } else if ($("#aboutus").length > 0) {
-        $("#jpNav #liAboutUs").addClass("active");
-    }
-}
