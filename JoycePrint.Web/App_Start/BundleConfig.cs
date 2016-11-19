58fc1cd530fa7ab4ff4a0765d9398d451f243ed3
polydegmon <polydegmon@gmail.com>
2016-11-18 23:11:55 -0700

    Help display triggered by focusing on the relevant inputs
    Composable bundles created so script that are not required on each page don't have to be loaded

--- JoycePrint.Web/App_Start/BundleConfig.cs
+++ JoycePrint.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Optimization;
+﻿using JoycePrint.Web.Extensions;
+using System.Web.Optimization;
 
 namespace JoycePrint.UI
 {
@@ -8,7 +9,9 @@ namespace JoycePrint.UI
         {
             bundles.IgnoreList.Clear();
 
-            AddBundles(bundles);
+            AddStyleBundles(bundles);
+
+            AddScriptComposableBundles(bundles);
 
             // Optimization for script and style bundles.
             // In debug mode the scripts will not be minified in the browser
@@ -19,27 +22,69 @@ namespace JoycePrint.UI
 #endif
         }
 
-        private static void AddBundles(BundleCollection bundles)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bundles"></param>
+        private static void AddScriptBundles(BundleCollection bundles)
         {
             // Create the script bundle
             // {version} can be used to specify the jQuery version, the token has to appear last in the file name
             bundles.Add(new ScriptBundle("~/js/joyceprintjs")
-                .Include("~/Scripts/jquery-{version}.js",
-                    "~/Scripts/materialize.min.js",
-                    "~/Scripts/joyceprint/joyceprint.js",
-                    "~/Scripts/joyceprint/jalidate.js",
-                    "~/Scripts/joyceprint/joyceprint-validation.js"
-                ));
+            .Include("~/Scripts/jquery-{version}.js",
+                "~/Scripts/materialize.min.js",
+                "~/Scripts/joyceprint/joyceprint.js",
+                "~/Scripts/joyceprint/jalidate.js",
+                "~/Scripts/joyceprint/joyceprint-validation.js"
+            ));            
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bundles"></param>
+        private static void AddScriptComposableBundles(BundleCollection bundles)
+        {
+            ComposableBundle<ScriptBundle> baseBundle = new ScriptBundle("~/js/joyceprintjs")
+                                            .AsComposable()
+                                            .Include("~/Scripts/jquery-{version}.js",
+                                                 "~/Scripts/materialize.min.js",
+                                                 "~/Scripts/joyceprint/joyceprint.js"
+                                            );
+
+            //ComposableBundle<ScriptBundle> validationBundle = new ScriptBundle("~/js/validationjs")
+            //                        .AsComposable()
+            //                        .Include("~/Scripts/joyceprint/jalidate.js",
+            //                            "~/Scripts/joyceprint/joyceprint-validation.js"
+            //                        );
+
+            bundles.Add(baseBundle);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bundles"></param>
+        private static void AddStyleBundles(BundleCollection bundles)
+        {
             // Create the external style bundle, using a bundle instead of a style bundle allows us to mix css and less files.
             // The less file is translated to css by the LessTransform class passed into the bundle
             bundles.Add(new Bundle("~/css/joyceprintcss",
-                new IBundleTransform[] { new LessTransform(), new CssMinify() })
-                .Include("~/Content/css/materialize.min.css",
-                    "~/Content/css/joyceprint/joyceprint.less")
-                );
+                                new IBundleTransform[] { new LessTransform(), new CssMinify() })
+                                .Include("~/Content/css/materialize.min.css",
+                                    "~/Content/css/joyceprint/joyceprint.less")
+                                    );
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="useCdn"></param>
+        /// <param name="bundles"></param>
+        /// <remarks>
+        /// This method requires a fall back for CDN failure in the form of a script tag on the view
+        /// for this reason, where available local files are cleaner
+        /// </remarks>
         private static void AddBundlesWithCdn(bool useCdn, BundleCollection bundles)
         {
             var MaterializeStyleCdn = @"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css";
---
+++ JoycePrint.Web/Extensions/ComposableBundle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+namespace JoycePrint.Web.Extensions
+{
+    public class ComposableBundle<T> where T : Bundle
+    {
+        private T _bundle;
+        private List<string> _virtualPaths = new List<string>();
+
+        public ComposableBundle(T bundle)
+        {
+            _bundle = bundle;
+        }
+
+        public string[] VirtualPaths
+        {
+            get { return _virtualPaths.ToArray(); }
+        }
+
+        public T Bundle
+        {
+            get { return _bundle; }
+        }
+
+        public ComposableBundle<T> Include(params string[] virtualPaths)
+        {
+            _virtualPaths.AddRange(virtualPaths);
+            _bundle.Include(virtualPaths);
+            return this;
+        }
+
+        public ComposableBundle<T> UseBundle(ComposableBundle<T> bundle)
+        {
+            var collection = new BundleCollection();
+            collection.Add(_bundle);
+
+            var resolver = new BundleResolver(collection);
+            List<string> content = resolver.GetBundleContents(_bundle.Path)?.ToList();
+
+            foreach (var virtualPath in bundle.VirtualPaths)
+            {
+                if (content.Contains(virtualPath)) continue;
+                _bundle.Include(virtualPath);
+            }
+
+            return this;
+        }
+    }
+
+    public static class BundleExtensions
+    {
+        public static ComposableBundle<T> AsComposable<T>(this T bundle) where T : Bundle
+        {
+            return new ComposableBundle<T>(bundle);
+        }
+
+        public static ComposableBundle<T> Add<T>(this BundleCollection bundles, ComposableBundle<T> bundle) where T : Bundle
+        {
+            bundles.Add(bundle.Bundle);
+            return bundle;
+        }
+    }
+}
