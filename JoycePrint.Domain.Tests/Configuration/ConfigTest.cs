8b86e735cd5209a7c6d779b16cddfe85442e58d6
Alan Joyce <polydegmon@gmail.com>
2017-04-14 15:31:53 -0600

    Updated the test config file to be created by copying the web config file into it's location when the test project is built

--- JoycePrint.Domain.Tests/Configuration/ConfigTest.cs
+++ JoycePrint.Domain.Tests/Configuration/ConfigTest.cs
@@ -12,7 +12,7 @@ namespace JoycePrint.Domain.Tests.Configuration
         [TestMethod]
         public void GetRecaptchaUrlTest()
         {
-            var expectedRecaptchaUrl = "https://www.google.com/recaptcha/api/siteverify";
+            const string expectedRecaptchaUrl = "https://www.google.com/recaptcha/api/siteverify";
 
             AssertAreEqual(expectedRecaptchaUrl, Config.RecaptchaUrl, "Recaptcha Url");
         }
@@ -37,6 +37,52 @@ namespace JoycePrint.Domain.Tests.Configuration
             const string expectedRecaptchaPublicKey = "6LcC2Q0UAAAAADtadrrG_FTRs82tvd2J1fOwK-KW";
 
             AssertAreEqual(expectedRecaptchaPublicKey, Config.RecaptchaPublicKey, "Recaptcha Public Key");
+        }
+
+        /// <summary>
+        /// This method will test the function that retrieves the Notification Header Success text
+        /// </summary>
+        [TestMethod]
+        public void GetNotificationHeaderSuccessTest()
+        {
+            const string expectedNotificationHeaderSuccess = "We have recieved your email and will get back to you shortly";
+
+            var t = Config.NotificationHeaderSuccess;
+
+            AssertAreEqual(expectedNotificationHeaderSuccess, Config.NotificationHeaderSuccess, "Notification Header Success");
+        }
+
+        /// <summary>
+        /// This method will test the function that retrieves the Notification Message Success text
+        /// </summary>
+        [TestMethod]
+        public void GetNotificationMessageSuccessTest()
+        {
+            const string expectedNotificationMessageSuccess = "Some body text";
+
+            AssertAreEqual(expectedNotificationMessageSuccess, Config.NotificationMessageSuccess, "Notification Messge Success");
+        }
+
+        /// <summary>
+        /// This method will test the function that retrieves the Notification Header Error text
+        /// </summary>
+        [TestMethod]
+        public void GetNotificationHeaderErrorTest()
+        {
+            const string expectedNotificationHeaderError = "An unexpected error has occurred";
+
+            AssertAreEqual(expectedNotificationHeaderError, Config.NotificationHeaderError, "Notification Header Error");
+        }
+
+        /// <summary>
+        /// This method will test the function that retrieves the Notification Message Error text
+        /// </summary>
+        [TestMethod]
+        public void GetNotificationMessageErrorTest()
+        {
+            const string expectedNotificationMessageError = "As the site is experiencing issues at the moment, please give us a call on +353-94-925-6876";
+
+            AssertAreEqual(expectedNotificationMessageError, Config.NotificationMessageError, "Notification Message Error");
         }
     }
 }
