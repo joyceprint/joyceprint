3426aa33560524357be61a5f81efda81b7bac5cf
Alan Joyce <polydegmon@gmail.com>
2017-05-07 19:38:25 -0600

    Page analysis action attribute created

---
+++ JoycePrint.Web/Attributes/PageAnalysis.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Common.Analytics.Tracking;
+using Common.Analytics;
+
+namespace JoycePrint.Web.Attributes
+{
+    public class PageAnalysis : ActionFilterAttribute
+    {
+        public string Name;
+        public string Host;
+        public string Title;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            var tracking = new Page
+            {
+                Name = Name,
+                Host = Host ?? filterContext.HttpContext.Request.Url.AbsoluteUri,
+                Title = Title                           
+            };
+
+            Analyzer.Instance.PageAnalysis(filterContext.HttpContext.ApplicationInstance.Context, tracking);
+        }
+    }
+}
