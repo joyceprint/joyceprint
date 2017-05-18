07d4ee12f876fa72ea90badd434a6e51a894646b
Alan Joyce <polydegmon@gmail.com>
2017-05-18 08:59:10 -0600

    Security updates

--- JoycePrint.Web/Global.asax.cs
+++ JoycePrint.Web/Global.asax.cs
@@ -25,15 +25,15 @@ namespace JoycePrint.Web
 
             // Register the bundles used in the application
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-       
+        }        
+
         /// <summary>
         /// 
         /// </summary>
         /// <remarks>
         /// Response Headers
         /// --------------------------------------------------------------------------------------------
-        /// Server: added by IIS. [ web.config ]
+        /// Server: added by IIS. [ TODO: NOT DONE YET ]
         /// X-AspNet-Version: added by System.Web.dll at the time of Flush in HttpResponse class [ web.config ]
         /// X-AspNetMvc-Version: Added by MvcHandler in System.Web.dll. [ MvcApplication.EnableSecurity ]
         /// X-Powered-By: added by IIS [ web.config ]
@@ -44,7 +44,7 @@ namespace JoycePrint.Web
             // This adds the X-FRAME-OPTIONS : DENY | SAMEORIGIN to the reponse.
             // This prevents click hyjacking by preventing the page from being loaded into an iframe
             // This is on by default in MVC 5, we have it here incase we ever change versions
-            AntiForgeryConfig.SuppressXFrameOptionsHeader = false;            
+            AntiForgeryConfig.SuppressXFrameOptionsHeader = false;
 
             // [Security - Application Hardening ]
             // Removing X-AspNetMvc-Version - Indicates that the website is "powered by MVC Version."
