6d2cdc0a39c1147faca297e655b3e4fffab8ad8c
Alan Joyce <polydegmon@gmail.com>
2017-05-22 18:01:14 -0600

    Anti forgery token working.
    This will be updated once a digital cert is available

--- JoycePrint.Web/Controllers/QuoteController.cs
+++ JoycePrint.Web/Controllers/QuoteController.cs
@@ -40,10 +40,8 @@ namespace JoycePrint.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                //var notificationType = model.SendEmail() ? NotificationType.Success : NotificationType.Failure;
-
-                var notificationType = NotificationType.Failure;
-
+                var notificationType = model.SendEmail() ? NotificationType.Success : NotificationType.Failure;
+                
                 // Create a new controller rather than using a redirect, a redirect will terminate the http request and return a 302
                 // A 302 response will break the ajax method that called this function
                 // Use this method of getting the controller as creating a new controller object will cause issues
